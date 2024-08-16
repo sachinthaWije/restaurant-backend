24c0367b278f05959c69493ba83cf25b6a837705
Sachintha Wijenayake <73356044+sachinthaWije@users.noreply.github.com>
2024-08-16 23:10:24 +0530

    load all offers

--- src/main/java/sachi/dev/restaurant/controller/OfferController.java
+++ src/main/java/sachi/dev/restaurant/controller/OfferController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import sachi.dev.restaurant.dto.OfferDTO;
 import sachi.dev.restaurant.service.OfferService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping()
 public class OfferController {
@@ -23,5 +25,10 @@ public class OfferController {
     public ResponseEntity<OfferDTO> updateOffer(@RequestBody  OfferDTO offerDTO,
                                                 @PathVariable String offerId) throws Exception {
         return new ResponseEntity<>(offerService.updateOffer(offerId,offerDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/staff/offer")
+    public ResponseEntity<List<OfferDTO>> getAllOffers() {
+        return new ResponseEntity<>(offerService.findAllOffers(), HttpStatus.OK);
     }
 }
